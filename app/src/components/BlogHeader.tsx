5d92a0ffe4406dc4d34d127d263ca490e42550b6
Roy Anger <roy@royanger.com>
2021-03-13 01:10:06 -0500

    A little styling and design on the Blog Header

--- app/src/components/BlogHeader.tsx
+++ app/src/components/BlogHeader.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react'
 
 const BlogHeader = () => {
-   return <p>BLOG HEADER </p>
+   return (
+      <div className="py-3 px-5 text-gray-90 mb-3">
+         <h1 className="text-gray-200 font-title text-5xl">Articles</h1>
+         <p className="text-gray-200 font-sans leading-loose text-xl">
+            My articles are a mix of things I've thought about, solutions to
+            problems I've dealt with and explanations of code I've worked on.{' '}
+         </p>
+      </div>
+   )
 }
 
 export default BlogHeader
