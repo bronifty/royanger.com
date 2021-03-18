51845055104337c56c07cb6292077564042a351a
Roy Anger <roy@royanger.com>
2021-03-18 19:02:12 -0400

    Trying to fix missing Tailwind class pt5

---
+++ app/out/_next/static/uVAnVvzD4MrTG6XkxpgxI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-809309282c9c7be878d8.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-82ec2e738d10e4548f61.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
