e9a0b026b4dc4392c00a9227b60feddd365177cc
Roy Anger <roy@royanger.com>
2021-03-25 02:43:09 -0400

    Fixed typo in test serverless functions

--- app/api/helloworld.js
+++ app/api/helloworld.js
@@ -4,5 +4,5 @@ import { red } from 'tailwindcss/colors'
 
 export default function helloworld(req, res) {
    res.statusCode = 200
-   red.json({ Hello: 'World!' })
+   res.json({ Hello: 'World!' })
 }
