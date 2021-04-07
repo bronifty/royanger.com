4a37dde3c712e279da5153852aeb3953ec0ebe40
Roy Anger <roy@royanger.com>
2021-04-06 20:11:57 -0400

    Setup up active page in header

--- app/components/Header/MenuItem.tsx
+++ app/components/Header/MenuItem.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 interface Props {
    link: string
@@ -7,10 +8,14 @@ interface Props {
 }
 
 const MenuItem = ({ link, title }: Props) => {
+   const router = useRouter()
+   const classes =
+      router.asPath === link ? 'lg:border-b border-lightblue-50' : 'lg:border-0'
+
    return (
       <>
          <Link href={link}>
-            <a className="border-b border-lightblue-100 lg:border-0">
+            <a className={`border-b border-lightblue-100 ${classes}`}>
                <button className=" px-10 py-5 lg:p-3 text-xl">{title}</button>
             </a>
          </Link>
