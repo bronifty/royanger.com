db8ea768c4128652a10f4a8bf78cb065ad3921f1
Roy Anger <roy@royanger.com>
2022-05-05 00:59:02 -0400

    Finished testing, using Heap.io. Cleaned up project

--- lib/constants/env.ts
+++ lib/constants/env.ts
@@ -1,3 +1,1 @@
-export const GTM = 'GTM-K427PD5'
-
 export const SITENAME = 'Roy Anger'
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,22 +3,13 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '../lib/context/themeContext'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer'
-// import TagMananger from 'react-gtm-module'
-import { GTM } from '../lib/constants/env'
 import '../styles/styles.scss'
 import '../styles/tailwind.css'
 
 import LogRocket from 'logrocket'
 LogRocket.init('kjcuh5/royangercom')
 
-// const tagManagerArgs = {
-//    gtmId: GTM,
-// }
-
 const MyApp = ({ Component, pageProps }: AppProps) => {
-   // React.useEffect(() => {
-   //    TagMananger.initialize(tagManagerArgs)
-   // })
    return (
       <ThemeProvider>
          <>
