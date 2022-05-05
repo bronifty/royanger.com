5940d2f12ad312d5cd18b578a8cdb6af90b7b630
Roy Anger <roy@royanger.com>
2022-05-04 21:42:15 -0400

    Trying to isolate error source

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '../lib/context/themeContext'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer'
-import TagMananger from 'react-gtm-module'
+// import TagMananger from 'react-gtm-module'
 import { GTM } from '../lib/constants/env'
 import '../styles/styles.scss'
 import '../styles/tailwind.css'
@@ -11,14 +11,14 @@ import '../styles/tailwind.css'
 import LogRocket from 'logrocket'
 LogRocket.init('kjcuh5/royangercom')
 
-const tagManagerArgs = {
-   gtmId: GTM,
-}
+// const tagManagerArgs = {
+//    gtmId: GTM,
+// }
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-   React.useEffect(() => {
-      TagMananger.initialize(tagManagerArgs)
-   })
+   // React.useEffect(() => {
+   //    TagMananger.initialize(tagManagerArgs)
+   // })
    return (
       <ThemeProvider>
          <>
